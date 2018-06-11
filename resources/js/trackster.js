6294980c2779163be41e19a469d912caae474e6e
BS_Designer <bs_designer@Xiaobins-MacBook-Pro.local>
2018-06-11 09:38:35 -0700

    Done.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,11 +1,19 @@
 var Trackster = {};
-const API_KEY = '8cef11f0f1a8a38f87d08c7d32a15299'
+const API_KEY = '8cef11f0f1a8a38f87d08c7d32a15299';
 
 $(document).ready(function() {
-  $('#button').click(function() {
+
+  $('#input').keydown(function(event){
+    if (event.which === 13) {
+      $('button').click();
+    }
+  });
+
+  $('button').click(function() {
     Trackster.searchTracksByTitle($('#input').val());
   });
 });
+
 
 
 Trackster.searchTracksByTitle = function(title) {
@@ -13,7 +21,7 @@ Trackster.searchTracksByTitle = function(title) {
       url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
       success: function(response) {
         Trackster.renderTracks(response.results.trackmatches.track);
-    }
+    },
   });
 };
 
@@ -23,16 +31,16 @@ Trackster.renderTracks = function(tracks) {
 
   for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
     var track = tracks[trackIndex];
-    var image = track.image[2]['#text'];
+    var image = track.image[1]['#text'];
+    var trackListeners = numeral(track.listeners).format('0,0');
     var html =
     '    <div class="row list align-items-center">' +
     '      <a href="' + track.url + '" target="_blank" class="col-1 offset-1"><i class="fas fa-play-circle"></i></a>' +
     '      <span class="col-3">' + track.name + '</span>' +
     '      <span class="col-2">' + track.artist + '</span>' +
-    '      <div class="col-2 image-container"><img src="' + image + '" alt=""></div>' +
-    '      <span class="col-2">' + track.listeners + '</span>' +
+    '      <div class="col-2"><img class="img-fluid" src="' + image + '" alt=""></div>' +
+    '      <span class="col-2">' + trackListeners + '</span>' +
     '    </div>' ;
-
     $('#list').append(html);
-  }
+  };
 };
